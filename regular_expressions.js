cc4ab46a9fa083ad46dc6bb8033e9a1dbe4bfd77
Dale <dellryuzi@gmail.com>
2017-01-04 20:50:59 +0700

    2 more regex

--- regular_expressions.js
+++ regular_expressions.js
@@ -1,17 +1,24 @@
 "use strict"
 // Determine whether a string contains a nomor KTP
 function has_ktp(string) {
+   if(string.match(/\d{3}-\d{2}-\d{4}/gi) ){
+     return true;
+   }else return false;
 
 }
 
 console.log("has_ktp returns true if it has what looks like a nomor KTP")
-console.log(has_ktp("please don't share this: 234-60-1422") == true)
+console.log(has_ktp("234-60-1422") == true)
 
 console.log("has_ktp returns false if it doesn't have a nomor KTP")
-console.log(has_ktp("please confirm your identity: XXX-XX-1422") == false)
 
-// Return the Social Security number from a string.
+console.log(has_ktp("XXX-XX-1422") == false)
+
+//Return the Social Security number from a string.
 function grab_ktp(string) {
+  if(string.match(/\d{3}-\d{2}-\d{4}/gi) ){
+    return true;
+  }else return null;
 
 }
 
@@ -23,6 +30,10 @@ console.log(grab_ktp("please confirm your identity: XXX-XX-1422") == null)
 
 // Return all of the Social Security numbers from a string.
 function grab_all_nomor_ktp(string) {
+  var emptyArr = [];
+  if(string.match(/\d{3}-\d{2}-\d{4}/gi) ){
+    return string;
+  }else return emptyArr;
 
 }
 
@@ -38,6 +49,11 @@ console.log(grab_all_nomor_ktp("please confirm your identity: XXX-XX-1422"))
 
 // Obfuscate all of the nomor KTP in a string. Example: XXX-XX-4430.
 function hide_all_nomor_ktp(string) {
+  if(string.match(/\d{3}-\d{2}-\d{4}/gi) ){
+    //return string.replace(/[^\d{4}]/g,'X');
+    //return string.replace(/\d{3}/g,'XXX');
+    return string.replace(/\d/g,'XXX');
+  }else return false;
 
 }
 
@@ -54,6 +70,9 @@ console.log(hide_all_nomor_ktp(string) == string)
 // Ensure all of the Social Security numbers use dashes for delimiters.
 // Example: 480.01.4430 and 480014430 would both be 480-01-4430.
 function format_nomor(string) {
+  if(string.match(/\d{3}-\d{2}-\d{4}/gi) ){
+    return string.replace(/[.]/g,'');
+  }else return emptyArr;
 
 }
 
