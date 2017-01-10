922f232aa36bfcaafb7f6d631f88cdc4298f6fc6
Wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-01-11 06:50:38 +0700

    Regex

--- regular_expressions.js
+++ regular_expressions.js
@@ -1,7 +1,7 @@
 "use strict"
 // Determine whether a string contains a nomor KTP
 function has_ktp(string) {
-
+	 return /\d{3}-\d{2}-/.test(string)
 }
 
 console.log("has_ktp returns true if it has what looks like a nomor KTP")
@@ -12,7 +12,8 @@ console.log(has_ktp("please confirm your identity: XXX-XX-1422") == false)
 
 // Return the Social Security number from a string.
 function grab_ktp(string) {
-
+  var num_ktp = string.match(/\d{3}-\d{2}-\d{4}/)
+  return num_ktp;
 }
 
 console.log("grab_ktp returns an nomor KTP if the string has an nomor KTP")
@@ -23,7 +24,8 @@ console.log(grab_ktp("please confirm your identity: XXX-XX-1422") == null)
 
 // Return all of the Social Security numbers from a string.
 function grab_all_nomor_ktp(string) {
-
+	var num_ktp = string.match(/\d{3}-\d{2}-/g)
+	return string.replace(num_ktp, "xxx-xx")
 }
 
 console.log("grab_all_nomor_ktp returns all nomor KTP if the string has any nomor KTP")
@@ -38,7 +40,14 @@ console.log(grab_all_nomor_ktp("please confirm your identity: XXX-XX-1422"))
 
 // Obfuscate all of the nomor KTP in a string. Example: XXX-XX-4430.
 function hide_all_nomor_ktp(string) {
-
+if(/\d+(?=\-)/g.test(string) === true){
+    //string = string.replace(/\d+(?=\-)/gi,"XXX-XX");
+    string = string.replace(/(\d{3})-(\d{2})/g,"XXX-XX");
+    return string;
+  }
+  else{
+    return string;
+  }
 }
 
 console.log("hide_all_nomor_ktp obfuscates any nomor KTP in the string")
@@ -54,7 +63,7 @@ console.log(hide_all_nomor_ktp(string) == string)
 // Ensure all of the Social Security numbers use dashes for delimiters.
 // Example: 480.01.4430 and 480014430 would both be 480-01-4430.
 function format_nomor(string) {
-
+ return string.replace(/(\d{3})\W*(\d{2})\W*(\d{4})/g, '$1-$2-$3')
 }
 
 console.log("format_nomor finds and reformat any nomor KTP in the string")
